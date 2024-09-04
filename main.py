07cfcb7a884c1a6932f852e38d16d532de2bdc8d
cecily.strong <cecily.strong@ucas-edu.net>
2024-09-04 10:42:47 -0600

    renamed main py

--- main.py
+++
@@ -1,7 +0,0 @@
-#Cecily Strong: SkillPractice: Setting, Resetting, and Resetting
-FC = 29
-S = 99
-G=(S-3)*2+3
-T=12
-SB=1
-print(FC+S+G+SB/12)
---
+++ setting,Resetting,andResetting/main.py
@@ -0,0 +1,8 @@
+#Cecily Strong: SkillPractice: Setting, Resetting, and Resetting
+FC = 29
+S = 99
+only1=15
+G=(S-only1)*2+only1
+T=12
+SB=1
+print(FC+S+G+SB/12)
