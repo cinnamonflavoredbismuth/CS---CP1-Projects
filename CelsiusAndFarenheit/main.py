a194db6f8884b27c88bacbcf971d86ce145bf050
cecily.strong <cecily.strong@ucas-edu.net>
2024-09-06 10:23:03 -0600

    Added print

---
+++ CelsiusAndFarenheit/main.py
@@ -0,0 +1,6 @@
+#Cecily Strong Celsius to Fahrenheit
+celsius=int(input("Whats the tempature in celsius?"))
+#int(celsius)
+farenheit=str((celsius * 9/5)+32)
+#str(farenheit)
+print("the tempature is", celsius, "degrees Celsius, and ", farenheit, "degrees Farenheit!")
