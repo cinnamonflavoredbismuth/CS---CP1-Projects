1081f4877e123a3dd34712f3afb3b494a488fad5
cecily.strong <cecily.strong@ucas-edu.net>
2024-09-06 10:10:40 -0600

    added variables

---
+++ whoAreYou/main.py
@@ -0,0 +1,7 @@
+nameinput=input("what is your name?")
+name=nameinput
+ageinput=int(input("what is your age?"))
+age=ageinput
+colorinput=input("what is your favorite color?")
+color=colorinput
+print("Hello", name, "I really like", color, "too! and", age, "is a really good age to be")
