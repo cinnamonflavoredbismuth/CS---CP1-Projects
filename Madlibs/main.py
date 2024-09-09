0e404d162799dc3e73d7b5d4acfab0267c953f59
cecily.strong <cecily.strong@ucas-edu.net>
2024-09-09 10:28:56 -0600

    MadLibs

---
+++ Madlibs/main.py
@@ -0,0 +1,13 @@
+#Cecily Strong period 2. assignment: ProficiencyTest: Madlib
+print("Welcome to MadLibs. Follow the instructions for a silly story.")
+verb=input("name a verb ending with ing")
+adjective=input("name an adjective" )
+adjective2=input("name an adjective" )
+color=input("name a color")
+place=input("name a place")
+animal=input("name an animal")
+print("It was a very", adjective, "day, when I was", verb,
+"down the street. I was very", adjective2, "when i was", verb, 
+"On my way to", place, "when suddenly, a", color, animal,
+"Jumped out at me! Well, I ran all the way to",place, 
+"when I saw that", animal, "worked there. I guess everyone needs to earn a living")
