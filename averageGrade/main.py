0c6c4c817848ff8c9c31229b67b06c5d32b044a9
cecily.strong <cecily.strong@ucas-edu.net>
2024-09-10 10:47:30 -0600

    added name

--- averageGrade/main.py
+++ averageGrade/main.py
@@ -1,11 +1,12 @@
-print("Hello! please type your grade percentages")
+#Cecily Strong SkillPractice: Average Grade
+print("Hello! please type your grade percentages so I can average them out")
 per1=int(input("period 1"))
 per2=int(input("period 2"))
 per3=int(input("period 3"))
 per6=int(input("period 6"))
 per7=int(input("period 7"))
 per8=int(input("period 8"))
-avg=per1+per2+per3+per6+per7+per8
-realavg=avg/6
-str(realavg)
-print("your average grade is", realavg)
+beginavg=per1+per2+per3+per6+per7+per8
+avg=beginavg/6
+str(avg)
+print("your average grade is", avg)
