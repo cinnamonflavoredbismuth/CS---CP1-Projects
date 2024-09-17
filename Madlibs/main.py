4430c1cdddb3099695b5ba997a8ca160571ea579
cecily.strong <cecily.strong@ucas-edu.net>
2024-09-17 10:16:10 -0600

    i tried to fix the code

--- Madlibs/main.py
+++ Madlibs/main.py
@@ -6,8 +6,5 @@ adjective2=input("name an adjective" )
 color=input("name a color")
 place=input("name a place")
 animal=input("name an animal")
-print("It was a very", adjective, "day, when I was", verb,
-"down the street. I was very", adjective2, "when i was", verb, 
-"On my way to", place, "when suddenly, a", color, animal,
-"Jumped out at me! Well, I ran all the way to",place, 
-"when I saw that", animal, "worked there. I guess everyone needs to earn a living")
+print("It was a very", adjective, "day, when I was", verb,"down the street. I was very", adjective2, "when i was", verb, "On my way to", place, "when suddenly, a", color, animal,"Jumped out at me! Well, I ran all the way to",place, "when I saw that", animal, "worked there. I guess everyone needs to earn a living")
+#theoretically it should print now?
