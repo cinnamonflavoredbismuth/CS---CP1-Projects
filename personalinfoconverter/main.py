d0a8288aef849c72abd8eefb18500f06cc3bd3bd
cecily.strong <cecily.strong@ucas-edu.net>
2024-09-18 10:16:21 -0600

    hello :3

---
+++ personalinfoconverter/main.py
@@ -0,0 +1,9 @@
+#Cecily Strong ProficiencyTest: Personal Information Converter
+name=input("What is your name?")
+age=input("What is your age?")
+height=input("what is your height in meters?")
+num=input("What is your favorite number?")
+age2=int(age)
+num2=int(num)
+height2=float(height)
+print("hello", name, ",you said you were", height, "meters tall, and that is", height2, "as a float, and your age is", age, """which is """, age2, "as an integer. Your favorite number is",num,"which is",num2,"as an integer")
