354f4178c58db88b049672237bcc06fcc24c2d14
cecily.strong <cecily.strong@ucas-edu.net>
2024-09-23 10:50:43 -0600

    cipher

---
+++ cipherWheel/main.py
@@ -0,0 +1,6 @@
+#Cecily Strong ProficiencyTest: Secret Cypher
+#I'm going to use this for the book im reading :)
+input("Caesar Cipher")
+plainText=input("type your cipher")
+def atbash (cipher):
+    print(cipher+4)
