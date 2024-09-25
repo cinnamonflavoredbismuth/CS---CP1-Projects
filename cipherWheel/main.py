4daad58f2728ff488adcbf6b8fc62ff543a6f753
cecily.strong <cecily.strong@ucas-edu.net>
2024-09-25 10:50:38 -0600

    afe3f

--- cipherWheel/main.py
+++ cipherWheel/main.py
@@ -1,42 +1,54 @@
 #Cecily Strong ProficiencyTest: Secret Cypher
 #I'm going to use this for the book im reading :)
 #yourText=input(
-print("Caesar Cipher")
-plainText=input("type your cipher")
-textList=[x for x in plainText]
-print(textList)
-i=0
-while i < len(textList):
-    def replace (str,num):
-       if textList[i]==str:
-          textList[i]=num
-    replace('a','d')
-    replace('b','e')
-    replace('c','f')
-    replace('d','g')
-    replace('e','h')
-    replace('f','i')
-    replace('g','j')
-    replace('h','k')
-    replace('i','l')
-    replace('j','m')
-    replace('k','n')
-    replace('l','o')
-    replace('m','p')
-    replace('n','q')
-    replace('o','r')
-    replace('p','s')
-    replace('q','t')
-    replace('r','u')
-    replace('s','v')
-    replace('t','w')
-    replace('u','y')
-    replace('v','z')
-    replace('w','a')
-    replace('x','b')
-    replace('y','c')
-    replace('z','d')
-    i += 1
+num=int(input("press 1 for Caesar Cipher, press 2 for atbash cipher, press 3 to translate a ceasar cipher to english"))
+if num==1:
+    plainText=input("type your cipher")
+    textList=[x for x in plainText]
+    print(textList)
+    i=0
+    for i, element in enumerate(textList):
+        oglet=ord[i]
+        shiftlet=oglet+4
+        chr(shiftlet)
+    i+=1
+
+if num==2:
+    plainText=input("type your cipher")
+    textList=[x for x in plainText]
+    print(textList)
+    i=0
+    while i < len(textList):
+        def replace (str,num):
+           if textList[i]==str:
+              textList[i]=num
+        replace('a','z')
+        replace('b','y')
+        replace('c','x')
+        replace('d','w')
+        replace('e','v')
+        replace('f','u')
+        replace('g','t')
+        replace('h','s')
+        replace('i','r')
+        replace('j','q')
+        replace('k','p')
+        replace('l','o')
+        replace('m','n')
+        replace('n','m')
+        replace('o','l')
+        replace('p','k')
+        replace('q','j')
+        replace('r','i')
+        replace('s','h')
+        replace('t','g')
+        replace('u','f')
+        replace('v','e')
+        replace('w','d')
+        replace('x','c')
+        replace('y','b')
+        replace('z','a')
+        i += 1
 
 textList.join(textList)
 print(textList)
