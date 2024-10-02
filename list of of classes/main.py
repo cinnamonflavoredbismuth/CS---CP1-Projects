6d4db146bda1f8492debfdd51fbc550f0083e7a2
cecily.strong <cecily.strong@ucas-edu.net>
2024-10-02 10:25:10 -0600

    woiagehwg

---
+++ list of of classes/main.py
@@ -0,0 +1,6 @@
+#Cecily Strong SkillPractice: List of classes
+classes=["Math",'CS 1400','Biology','Student Council',"Geography","Drawing","English"]
+s=0
+while s < len(classes):
+    print(classes[s])
+    s+=1
