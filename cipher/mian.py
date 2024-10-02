1f59a36d5f625a68e7c8966dfd2d0a11f91c7802
cecily.strong <cecily.strong@ucas-edu.net>
2024-10-02 10:21:14 -0600

    XLI$REQIW$FMPP0$FMPP$GMTLIV

--- cipher/mian.py
+++ cipher/mian.py
@@ -1,7 +1,13 @@
 #Cecily Strong ProficiencyTest: Secret Cypher
+changeby=int(input("How many do you want the cipher to shift by?"))
 words=input("type your cipher")
 wordslist=list(words)
 
-for s in wordslist:
-        wordslist.append(ord(s))
-print(wordslist)
+s=0
+while s < len(wordslist):
+        wordslist[s]=ord(wordslist[s])
+        wordslist[s]+=changeby
+        wordslist[s]=chr(wordslist[s])
+        s+=1
+
+print("".join(wordslist))
