631d14339317efc5941c653d7492acdeccca0427
cecily.strong <cecily.strong@ucas-edu.net>
2024-10-04 10:47:21 -0600

    ehgoiwg

--- Shopping List Manager/main.py
+++ Shopping List Manager/main.py
@@ -1,28 +1,26 @@
 #Cecily Strong ProficiencyTest: Shopping list manager
-#def remove():
-#        remove=input("what would you like to remove?")
-#        shoppinglist.remove(remove)
-#       print(shoppinglist)
-#def add():
-#        add=input("what would you like to add?")
-#        shoppinglist.append(add)
-#        print(shoppinglist)
-shoppinglist=[0]
 
-#action=0
-#hile action is 3:
-action = input("""What would you like to do?
+shoppinglist=[]
+s=0
+while True:
+    action = input("""What would you like to do?
+                   
                           Enter 1 to add item
+                   
                           Enter 2 to remove item
+                   
                           Enter 3 to leave the list:\n""")
-if action =="1":
-    remove=input("what would you like to remove?")
-    shoppinglist.remove(remove)
-    print(shoppinglist)
-elif action =="2":
-    remove=input("what would you like to remove?")
-    shoppinglist.remove(remove)
-    print(shoppinglist)
-else:
-    print
-    print("Have a nice day!")
+    if action =="1":
+        add=input("what would you like to add?")
+        shoppinglist.append(add)
+        print(', '.join(shoppinglist))
+
+ 
+    elif action =="2":
+        remove=input("what would you like to remove?")
+        shoppinglist.remove(remove)
+        print(', '.join(shoppinglist))
+
+    else:
+        print("Have a nice day!")
+        break
