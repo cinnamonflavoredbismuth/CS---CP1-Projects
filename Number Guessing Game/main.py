f38d6436d812a2a12517dba93b9a1e9b703544e6
cecily.strong <cecily.strong@ucas-edu.net>
2024-10-09 10:51:02 -0600

    gwsgfgeqgf

--- Number Guessing Game/main.py
+++ Number Guessing Game/main.py
@@ -1,3 +1,12 @@
 #Cecily Strong RAID: Number Guessing Game
 import random
 number= random.randint(1,10)
+userNum=0
+while userNum!=number:
+    userNum=int(input("what is my number?"))
+    if userNum>number:
+        print("too high!")
+    else:
+        print("too low!")
+else:
+    print("congradulations! You got it right!")
