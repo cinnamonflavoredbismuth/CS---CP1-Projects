9b452b84bee2b21eb491dbbe1229d21e86d90c9c
cecily.strong <cecily.strong@ucas-edu.net>
2024-10-10 10:49:12 -0600

    why is the word palendrome not a palendrome?

---
+++ Palendrome Checker/main.py
@@ -0,0 +1,6 @@
+#Cecily Strong SkillPractice: Palindrome
+palendrome=input("type a word and I'll check if its a palendrome")
+if palendrome==(palendrome[::-1]):
+    print("this is a palendrome!")
+else:
+    print("this is not a palendrome")
