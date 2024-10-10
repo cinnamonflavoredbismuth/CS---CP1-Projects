eb3ecc00a2ffc7de4a2df1e176c7526014ceee35
cecily.strong <cecily.strong@ucas-edu.net>
2024-10-10 10:24:07 -0600

    gsg

--- Number Guessing Game/main.py
+++ Number Guessing Game/main.py
@@ -1,12 +1,21 @@
 #Cecily Strong RAID: Number Guessing Game
 import random
-number= random.randint(1,10)
-userNum=0
-while userNum!=number:
-    userNum=int(input("what is my number?"))
-    if userNum>number:
-        print("too high!")
+play='yes'
+while play=='yes':
+    number= random.randint(1,10)
+    userNum=0
+    while userNum!=number:
+        userNum=int(input("I picked a number between 1 and 10, what is my number?"))
+        if userNum>number:
+            print("too high!")
+        else:
+            if userNum<number:
+                print("too low!")
     else:
-        print("too low!")
-else:
-    print("congradulations! You got it right!")
+        print("congradulations! You got it right!")
+        playagain=input("play again? Y/N")
+        if playagain==('Y' or 'y'or'yes'):
+            play='yes'
+        else:
+            play='no'
+print("thank you for playing")
