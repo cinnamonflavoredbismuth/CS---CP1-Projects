a2f7ed11f047fa3d26cfa30957f319a389b9217a
cecily.strong <cecily.strong@ucas-edu.net>
2024-10-21 10:18:58 -0600

    welp, i tried

--- Multiplycation table/main.py
+++ Multiplycation table/main.py
@@ -7,10 +7,9 @@ y=1
 x=0
 
 for x in range (12):
-    for n in range(0,13*y,y):
+    for n in range(1,13*y,y):
         underline = "\033[4m" 
         reset = "\033[0m"
-        print(f"{underline}",n,"{underline}"end='|')
+        print(f"{underline}",n,end='|')
     print('')
     y+=1
-
