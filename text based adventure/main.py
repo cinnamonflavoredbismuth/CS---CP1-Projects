5d5b0074d372582b58a03dee08dc73b75c9a42c0
cinnamonflavoredbismuth <cecily.strong@ucas-edu.net>
2024-11-19 10:51:04 -0700

    vzvdzv

---
+++ text based adventure/main.py
@@ -0,0 +1,6 @@
+#Cecily Strong ProficiencyTest: Plan your User Interface
+print("You're losing your memory! quick, try to remember...")
+name=input("What is your name?")
+town=input('From where do you hail?')
+fulltitle=(f'{name} of {town}')
+print(fulltitle)
